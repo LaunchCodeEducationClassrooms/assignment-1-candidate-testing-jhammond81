dd2a76c0a3752a10a78e4b26e0ff2238c8f9de24
bob hammond <jbrhammond@gmail.com>
2021-02-22 02:19:33 +0000

    took space in string away

--- candidate-testing.js
+++ candidate-testing.js
@@ -4,7 +4,7 @@ const input = require('readline-sync');
 let namePrompt="What is your name? ";
 
 // TODO 1.1a: Define candidateName // 
-let candidateName=" ";
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space? ";
 let correctAnswer="Sally Ride.";
